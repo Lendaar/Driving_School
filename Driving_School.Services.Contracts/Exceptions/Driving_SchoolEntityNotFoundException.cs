ef6ff8ce656ffd1d88e64e2f814a759fa3162c84
voronenkov04@mail.ru <voronenkov04@mail.ru>
2023-11-29 17:01:10 +0300

    Добавил классы ошибок

---
+++ Driving_School.Services.Contracts/Exceptions/Driving_SchoolEntityNotFoundException.cs
@@ -0,0 +1,18 @@
+﻿using Driving_School.Services.Contracts.Exceptions;
+
+namespace Driving_School.Services.Contracts.Exceptions
+{
+    /// <summary>
+    /// Запрашиваемая сущность не найдена
+    /// </summary>
+    public class Driving_SchoolEntityNotFoundException<TEntity> : Driving_SchoolNotFoundException
+    {
+        /// <summary>
+        /// Инициализирует новый экземпляр <see cref="Driving_SchoolEntityNotFoundException{TEntity}"/>
+        /// </summary>
+        public Driving_SchoolEntityNotFoundException(Guid id)
+            : base($"Сущность {typeof(TEntity)} c id = {id} не найдена.")
+        {
+        }
+    }
+}
---
+++ Driving_School.Services.Contracts/Exceptions/Driving_SchoolException.cs
@@ -0,0 +1,20 @@
+﻿namespace Driving_School.Services.Contracts.Exceptions
+{
+    /// <summary>
+    /// Базовый класс исключений
+    /// </summary>
+    public abstract class Driving_SchoolException : Exception
+    {
+        /// <summary>
+        /// Инициализирует новый экземпляр <see cref="Driving_SchoolException"/> без параметров
+        /// </summary>
+        protected Driving_SchoolException() { }
+
+        /// <summary>
+        /// Инициализирует новый экземпляр <see cref="Driving_SchoolException"/> с указанием
+        /// сообщения об ошибке
+        /// </summary>
+        protected Driving_SchoolException(string message)
+            : base(message) { }
+    }
+}
