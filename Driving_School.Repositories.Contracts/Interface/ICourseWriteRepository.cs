4dd1f497dd19016df5c4c2f2afb96c971d9591b5
voronenkov04@mail.ru <voronenkov04@mail.ru>
2023-11-29 16:06:20 +0300

    Создал интерфейсы для репозиториев записи

---
+++ Driving_School.Repositories.Contracts/Interface/ICourseWriteRepository.cs
@@ -0,0 +1,11 @@
+﻿using Driving_School.Context.Contracts.Models;
+
+namespace Driving_School.Repositories.Contracts
+{
+    /// <summary>
+    /// Репозиторий записи <see cref="Course"/>
+    /// </summary>
+    public interface ICourseWriteRepository : IRepositoryWriter<Course>
+    {
+    }
+}
