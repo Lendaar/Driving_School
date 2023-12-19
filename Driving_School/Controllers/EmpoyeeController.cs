f09e59891d3a9127771565a43c5eae0817063e03
voronenkov04@mail.ru <voronenkov04@mail.ru>
2023-12-19 15:27:08 +0300

    Написал CRUD

--- Driving_School/Controllers/EmpoyeeController.cs
+++ Driving_School/Controllers/EmpoyeeController.cs
@@ -2,6 +2,9 @@
 using Microsoft.AspNetCore.Mvc;
 using Driving_School.Api.Models;
 using Driving_School.Services.Contracts.Interface;
+using Driving_School.Api.ModelsRequest.Employee;
+using Driving_School.Services.Contracts.RequestModels;
+using System.ComponentModel.DataAnnotations;
 
 namespace Driving_School.Api.Controllers
 {
@@ -38,7 +41,7 @@ namespace Driving_School.Api.Controllers
         /// </summary>
         [HttpGet("{id:guid}")]
         [ProducesResponseType(typeof(IEnumerable<EmployeeResponse>), StatusCodes.Status200OK)]
-        public async Task<IActionResult> GetById(Guid id, CancellationToken cancellationToken)
+        public async Task<IActionResult> GetById([Required]Guid id, CancellationToken cancellationToken)
         {
             var item = await employeeService.GetByIdAsync(id, cancellationToken);
             if (item == null)
@@ -46,6 +49,41 @@ namespace Driving_School.Api.Controllers
                 return NotFound($"Не удалось найти работника с идентификатором {id}");
             }
             return Ok(mapper.Map<EmployeeResponse>(item));
+        }
+
+        /// <summary>
+        /// Создаёт нового рабочего
+        /// </summary>
+        [HttpPost]
+        [ProducesResponseType(typeof(EmployeeResponse), StatusCodes.Status200OK)]
+        public async Task<IActionResult> Create(CreateEmployeeRequest request, CancellationToken cancellationToken)
+        {
+            var employeeRequestModel = mapper.Map<EmployeeRequestModel>(request);
+            var result = await employeeService.AddAsync(employeeRequestModel, cancellationToken);
+            return Ok(mapper.Map<EmployeeResponse>(result));
+        }
+
+        /// <summary>
+        /// Редактирует имеющищегося рабочего
+        /// </summary>
+        [HttpPut]
+        [ProducesResponseType(typeof(EmployeeResponse), StatusCodes.Status200OK)]
+        public async Task<IActionResult> Edit(EmployeeRequest request, CancellationToken cancellationToken)
+        {
+            var model = mapper.Map<EmployeeRequestModel>(request);
+            var result = await employeeService.EditAsync(model, cancellationToken);
+            return Ok(mapper.Map<EmployeeResponse>(result));
+        }
+
+        /// <summary>
+        /// Удаляет имеющийегося рабочего по id
+        /// </summary>
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IActionResult> Delete(Guid id, CancellationToken cancellationToken)
+        {
+            await employeeService.DeleteAsync(id, cancellationToken);
+            return Ok();
         }
     }
 }
--- Driving_School/Controllers/LessonController.cs
+++ Driving_School/Controllers/LessonController.cs
@@ -2,6 +2,11 @@
 using Driving_School.Api.Models;
 using Driving_School.Services.Contracts.Interface;
 using AutoMapper;
+using System.ComponentModel.DataAnnotations;
+using Driving_School.Api.ModelsRequest.Place;
+using Driving_School.Services.Contracts.RequestModels;
+using Driving_School.Services.Implementations;
+using Driving_School.Api.ModelsRequest.Lesson;
 
 namespace Driving_School.Api.Controllers
 {
@@ -38,7 +43,7 @@ namespace Driving_School.Api.Controllers
         /// </summary>
         [HttpGet("{id:guid}")]
         [ProducesResponseType(typeof(IEnumerable<LessonResponse>), StatusCodes.Status200OK)]
-        public async Task<IActionResult> GetById(Guid id, CancellationToken cancellationToken)
+        public async Task<IActionResult> GetById([Required] Guid id, CancellationToken cancellationToken)
         {
             var item = await lessonService.GetByIdAsync(id, cancellationToken);
             if (item == null)
@@ -46,6 +51,41 @@ namespace Driving_School.Api.Controllers
                 return NotFound($"Не удалось найти занятие с идентификатором {id}");
             }
             return Ok(mapper.Map<LessonResponse>(item));
+        }
+
+        /// <summary>
+        /// Создаёт новое занятие
+        /// </summary>
+        [HttpPost]
+        [ProducesResponseType(typeof(LessonResponse), StatusCodes.Status200OK)]
+        public async Task<IActionResult> Create(CreateLessonRequest request, CancellationToken cancellationToken)
+        {
+            var lessonRequestModel = mapper.Map<LessonRequestModel>(request);
+            var result = await lessonService.AddAsync(lessonRequestModel, cancellationToken);
+            return Ok(mapper.Map<LessonResponse>(result));
+        }
+
+        /// <summary>
+        /// Редактирует имеющееся занятие
+        /// </summary>
+        [HttpPut]
+        [ProducesResponseType(typeof(LessonResponse), StatusCodes.Status200OK)]
+        public async Task<IActionResult> Edit(LessonRequest request, CancellationToken cancellationToken)
+        {
+            var model = mapper.Map<LessonRequestModel>(request);
+            var result = await lessonService.EditAsync(model, cancellationToken);
+            return Ok(mapper.Map<LessonResponse>(result));
+        }
+
+        /// <summary>
+        /// Удаляет имеющееся занятие
+        /// </summary>
+        [HttpDelete("{id:guid}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IActionResult> Delete(Guid id, CancellationToken cancellationToken)
+        {
+            await lessonService.DeleteAsync(id, cancellationToken);
+            return Ok();
         }
     }
 }
