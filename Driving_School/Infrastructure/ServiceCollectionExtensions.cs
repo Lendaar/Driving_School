2078c4a2389e1c19c6d39aa3aad7b8b1fe856e89
voronenkov04@mail.ru <voronenkov04@mail.ru>
2023-12-19 15:26:09 +0300

    Добавил регистрацию добавленных автомапперов, DateTimeProvider и DbWriterContext

--- Driving_School/Infrastructure/ServiceCollectionExtensions.cs
+++ Driving_School/Infrastructure/ServiceCollectionExtensions.cs
@@ -1,4 +1,6 @@
-﻿using Driving_School.Common;
+﻿using Driving_School.Api.Infrastructure;
+using Driving_School.Common;
+using Driving_School.Common.Entity.InterfaceDB;
 using Driving_School.Context;
 using Driving_School.Repositories;
 using Driving_School.Services;
@@ -10,6 +12,10 @@ namespace Driving_School.Infrastructure
     { 
         public static void AddDependences(this IServiceCollection services)
         {
+            services.AddTransient<IDateTimeProvider, DateTimeProvider>();
+            services.AddTransient<IDbWriterContext, DbWriterContext>();
+            services.RegisterAutoMapperProfile<ApiAutoMapperProfile>();
+
             services.RegisterModule<ServiceModule>();
             services.RegisterModule<ContextModule>();
             services.RegisterModule<RepositoriesModule>();
