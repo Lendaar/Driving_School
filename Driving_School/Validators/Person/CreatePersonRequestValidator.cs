4bdeee2da5a6e286cbf786a3f78e7a59f45aed6e
voronenkov04@mail.ru <voronenkov04@mail.ru>
2023-12-22 16:01:09 +0300

    Добавил валидатор "Курса", "Персоны" и "Работника"

---
+++ Driving_School/Validators/Person/CreatePersonRequestValidator.cs
@@ -0,0 +1,46 @@
+﻿using Driving_School.Api.ModelsRequest.Person;
+using FluentValidation;
+
+namespace Driving_School.Api.Validators.Person
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class CreatePersonRequestValidator : AbstractValidator<CreatePersonRequest>
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public CreatePersonRequestValidator()
+        {
+            RuleFor(x => x.LastName)
+                .NotNull()
+                .NotEmpty()
+                .WithMessage("Фамилия не должна быть пустой или null")
+                .MaximumLength(80)
+                .WithMessage("Фамилия больше 80 символов");
+
+            RuleFor(x => x.FirstName)
+                .NotNull()
+                .NotEmpty()
+                .WithMessage("Имя не должно быть пустым или null")
+                .MaximumLength(80)
+                .WithMessage("Имя больше 80 символов");
+
+            RuleFor(x => x.DateOfBirthday)
+                .NotNull()
+                .NotEmpty()
+                .WithMessage("дата рождения не должна быть пустой или null");
+
+            RuleFor(x => x.Passport)
+                .NotNull()
+                .NotEmpty()
+                .WithMessage("Номер паспорта не должен быть пустой или null");
+
+            RuleFor(x => x.Phone)
+                .NotNull()
+                .NotEmpty()
+                .WithMessage("Телефон не должен быть пустым или null");
+        }
+    }
+}
---
+++ Driving_School/Validators/Person/PersonRequestValidator.cs
@@ -0,0 +1,51 @@
+﻿using Driving_School.Api.ModelsRequest.Person;
+using FluentValidation;
+
+namespace Driving_School.Api.Validators.Person
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class PersonRequestValidator : AbstractValidator<PersonRequest>
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public PersonRequestValidator()
+        {
+            RuleFor(x => x.Id)
+               .NotNull()
+               .NotEmpty()
+               .WithMessage("Id не должен быть пустым или null");
+
+            RuleFor(x => x.LastName)
+                .NotNull()
+                .NotEmpty()
+                .WithMessage("Фамилия не должна быть пустой или null")
+                .MaximumLength(80)
+                .WithMessage("Фамилия больше 80 символов");
+
+            RuleFor(x => x.FirstName)
+                .NotNull()
+                .NotEmpty()
+                .WithMessage("Имя не должно быть пустым или null")
+                .MaximumLength(80)
+                .WithMessage("Имя больше 80 символов");
+
+            RuleFor(x => x.DateOfBirthday)
+                .NotNull()
+                .NotEmpty()
+                .WithMessage("дата рождения не должна быть пустой или null");
+
+            RuleFor(x => x.Passport)
+                .NotNull()
+                .NotEmpty()
+                .WithMessage("Номер паспорта не должен быть пустой или null");
+
+            RuleFor(x => x.Phone)
+                .NotNull()
+                .NotEmpty()
+                .WithMessage("Телефон не должен быть пустым или null");
+        }
+    }
+}
