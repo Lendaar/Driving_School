9c47007fa94689eb9a4bd9358d2cccb135759c6a
voronenkov04@mail.ru <voronenkov04@mail.ru>
2023-12-22 15:24:57 +0300

    Добавил валидатор "Плодадки" и "Транспорта"

---
+++ Driving_School/Validators/Transport/CreateTransportRequestValidator.cs
@@ -0,0 +1,35 @@
+﻿using FluentValidation;
+using Driving_School.Api.ModelsRequest.Transport;
+
+namespace Driving_School.Api.Validators.Transport
+{
+    /// <summary>
+    /// Валидатор класса <see cref="CreateTransportRequest"/>
+    /// </summary>
+    public class CreateTransportRequestValidator : AbstractValidator<CreateTransportRequest>
+    {
+        /// <summary>
+        /// Инициализирую <see cref="CreateTransportRequestValidator"/>
+        /// </summary>
+        public CreateTransportRequestValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotNull()
+                .NotEmpty()
+                .WithMessage("Наименование транспорта не должно быть пустым или null")
+                .MaximumLength(50)
+                .WithMessage("Наименование транспорта больше 50 символов");
+
+            RuleFor(x => x.Number)
+                .NotNull()
+                .NotEmpty()
+                .WithMessage("Гос номер не должен быть пустым или null")
+                .MaximumLength(10)
+                .WithMessage("Гос номер больше 10 символов");
+
+            RuleFor(x => x.GSBType)
+                .NotNull()
+                .WithMessage("Тип КПП не должен быть null");
+        }
+    }
+}
---
+++ Driving_School/Validators/Transport/TransportRequestValidator.cs
@@ -0,0 +1,40 @@
+﻿using Driving_School.Api.ModelsRequest.Transport;
+using FluentValidation;
+
+namespace Driving_School.Api.Validators.Transport
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class TransportRequestValidator : AbstractValidator<TransportRequest>
+    {
+        /// <summary>
+        /// ctor
+        /// </summary>
+        public TransportRequestValidator()
+        {
+            RuleFor(x => x.Id)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("Id не должно быть пустым или null");
+
+            RuleFor(x => x.Name)
+                .NotNull()
+                .NotEmpty()
+                .WithMessage("Наименование транспорта не должно быть пустым или null")
+                .MaximumLength(50)
+                .WithMessage("Наименование транспорта больше 50 символов");
+
+            RuleFor(x => x.Number)
+                .NotNull()
+                .NotEmpty()
+                .WithMessage("Гос номер не должен быть пустым или null")
+                .MaximumLength(10)
+                .WithMessage("Гос номер больше 10 символов");
+
+            RuleFor(x => x.GSBType)
+                .NotNull()
+                .WithMessage("Тип КПП не должен быть null");
+        }
+    }
+}
