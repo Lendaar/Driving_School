468dd5c5bfa3d57cef6472c02200beaebf2be56f
voronenkov04@mail.ru <voronenkov04@mail.ru>
2023-12-26 14:54:06 +0300

    Добавил проект для тестов с БД на памяти

---
+++ Driving_School.Context.Tests/Driving_SchoolContextInMemory.cs
@@ -0,0 +1,55 @@
+﻿using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Driving_School.Common.Entity.InterfaceDB;
+
+namespace Driving_School.Context.Tests
+{
+    /// <summary>
+    /// Класс <see cref="Driving_SchoolContext"/> для тестов с базой в памяти. Один контекст на тест
+    /// </summary>
+    public abstract class Driving_SchoolContextInMemory : IAsyncDisposable
+    {
+        protected readonly CancellationToken CancellationToken;
+        private readonly CancellationTokenSource cancellationTokenSource;
+
+        /// <summary>
+        /// Контекст <see cref="Driving_SchoolContext"/>
+        /// </summary>
+        protected Driving_SchoolContext Context { get; }
+
+        /// <inheritdoc cref="IUnitOfWork"/>
+        protected IUnitOfWork UnitOfWork => Context;
+
+        /// <inheritdoc cref="IDbRead"/>
+        protected IDbRead Reader => Context;
+
+        protected IDbWriterContext WriterContext => new TestWriterContext(Context, UnitOfWork);
+
+        protected Driving_SchoolContextInMemory()
+        {
+            cancellationTokenSource = new CancellationTokenSource();
+            CancellationToken = cancellationTokenSource.Token;
+            var optionsBuilder = new DbContextOptionsBuilder<Driving_SchoolContext>()
+                .UseInMemoryDatabase($"MoneronTests{Guid.NewGuid()}")
+                .ConfigureWarnings(w => w.Ignore(InMemoryEventId.TransactionIgnoredWarning));
+            Context = new Driving_SchoolContext(optionsBuilder.Options);
+        }
+
+        /// <inheritdoc cref="IDisposable"/>
+        public async ValueTask DisposeAsync()
+        {
+            cancellationTokenSource.Cancel();
+            cancellationTokenSource.Dispose();
+            try
+            {
+                await Context.Database.EnsureDeletedAsync();
+                await Context.DisposeAsync();
+            }
+            catch (ObjectDisposedException ex)
+            {
+                Trace.TraceError(ex.Message);
+            }
+        }
+    }
+}
