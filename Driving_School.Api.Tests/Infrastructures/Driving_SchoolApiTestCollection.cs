7d2c3827f3badbfc7c85b68d2d1613e41df51722
voronenkov04@mail.ru <voronenkov04@mail.ru>
2023-12-29 16:54:27 +0300

    Инфраструктура для интеграционнных тестов

---
+++ Driving_School.Api.Tests/Infrastructures/Driving_SchoolApiTestCollection.cs
@@ -0,0 +1,10 @@
+﻿using Xunit;
+
+namespace Driving_School.Api.Tests.Infrastructures
+{
+    [CollectionDefinition(nameof(Driving_SchoolApiTestCollection))]
+    public class Driving_SchoolApiTestCollection
+        : ICollectionFixture<Driving_SchoolApiFixture>
+    {
+    }
+}
---
+++ Driving_School.Api.Tests/Infrastructures/WebHostBuilderHelper.cs
@@ -0,0 +1,19 @@
+﻿namespace Driving_School.Api.Tests.Infrastructures
+{
+    static internal class WebHostBuilderHelper
+    {
+        /// <summary>
+        /// Конфигурирование IWebHostBuilder
+        /// </summary>
+        public static void ConfigureTestAppConfiguration(this IWebHostBuilder builder)
+        {
+            builder.ConfigureAppConfiguration((_, config) =>
+            {
+                var projectDir = Directory.GetCurrentDirectory();
+                var configPath = Path.Combine(projectDir,
+                    $"appsettings.{CustomWebApplicationFactory.EnvironmentName}.json");
+                config.AddJsonFile(configPath).AddEnvironmentVariables();
+            });
+        }
+    }
+}
